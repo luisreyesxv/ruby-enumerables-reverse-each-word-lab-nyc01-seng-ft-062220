6aa99c3a57bd4f2fcee79ff1d2c0334c1ffbda37
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:37:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (sentence)
     broken_sentence = sentence.split
     answer = ""
     broken_sentence.each do |word|
-      answer << word
+      answer << "#{word} "
     end
     answer
 
