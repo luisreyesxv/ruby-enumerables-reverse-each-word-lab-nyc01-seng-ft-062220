2110145ad38b88fd31c28491214a087aa7a1a672
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:36:24 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+
+def reverse_each_word (sentence)
+    broken_sentence = sentence.split
+    answer = ""
+    broken_sentence.each do |word|
+      answer << word
+    end
+    answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end  
