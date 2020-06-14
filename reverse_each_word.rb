16f7d451ea45e06ba81658d0231c234882c39c51
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:48:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word (sentence)
     broken_sentence = sentence.split
     answer = ""
-    broken_sentence.collect{|word| word.reverse}.to_s
+    broken_sentence.collect{|word| word.reverse}.join
       #if word == broken_sentence[-1]
       #  answer << "#{word.reverse}"
       #else
