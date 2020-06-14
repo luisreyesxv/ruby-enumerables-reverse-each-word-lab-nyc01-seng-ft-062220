8ebffde40af68011b9e957c2b3e99d0a0fae0583
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:49:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,15 @@
 
 def reverse_each_word (sentence)
     broken_sentence = sentence.split
-    answer = ""
-    broken_sentence.collect{|word| word.reverse}.join
+    
+    answer =broken_sentence.collect{|word| word.reverse}
       #if word == broken_sentence[-1]
       #  answer << "#{word.reverse}"
       #else
       #  answer << "#{word.reverse} "
       #end
-    end
+    answer.join  
+end
 
 
 
