9b9e67577721b053a72e7e85f676829cbc231ccc
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:38:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 
 def reverse_each_word (sentence)
-    broken_sentence = sentence.split
+    
     answer = ""
-    broken_sentence.each do |word|
+    sentence.each do |word|
       answer << "#{word.reverse} "
     end
     answer
