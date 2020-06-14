6fb4f3af5b56ae6f9d69995b264c7181ad450d1a
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:55:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-require 'pry'
+
 def reverse_each_word (sentence)
     broken_sentence = sentence.split
 
-    answer = broken_sentence.collect{|word| word.reverse}.join(" ")
+    broken_sentence.collect{|word| word.reverse}.join(" ")
 
 
 
